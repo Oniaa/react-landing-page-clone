c3190ebffdf97a0d94fad72b9cfc4f5a4aebaacd
ony <ogbuebele.ony@hotmail.com>
2023-05-09 14:43:12 +0200

    Create Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.sass';
+
+export default function App() {
+  return (
+    <>
+      <header>Header</header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </>
+  );
+}
