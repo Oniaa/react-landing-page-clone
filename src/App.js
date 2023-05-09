b22209e65c854aff57c58c0149367eb2f1c9b569
ony <ogbuebele.ony@hotmail.com>
2023-05-09 18:04:49 +0200

    Add part of header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import styles from './App.module.sass';
+import styles from './App.module.scss';
 
 export default function App() {
   return (
     <>
-      <header>Header</header>
-      <main>Main</main>
+      <header>
+        <nav className={styles.navigationBar}>
+          <a href="/#">
+            <span>Crosby</span>
+          </a>
+          <ul>
+            <li>
+              <a href="/#">Shop</a>
+            </li>
+            <li>
+              <a href="/#">Our Story</a>
+            </li>
+            <li>
+              <a href="/#">Journal</a>
+            </li>
+            <li>
+              <a href="/#">Contact</a>
+            </li>
+          </ul>
+          <div>
+            <a href="/#">Icon</a>
+            <a href="/#">Icon</a>
+            <a href="/#">Icon</a>
+            <a href="/#">Icon</a>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section className={styles.heroPage}>
+          <h1>
+            Plants and Pots
+            <br />
+            For Your Home
+          </h1>
+          <a href="/#">
+            <span>Shop Now</span>
+          </a>
+        </section>
+      </main>
       <footer>Footer</footer>
     </>
   );
